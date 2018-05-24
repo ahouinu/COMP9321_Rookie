55e853e7d8d50da7aa6f6eb932a5e00af9939ae2
Miro <>
2018-05-24 17:33:05 +1000

    Typo_checker added.
    etc:
    input: Kingsfaok --> "Do you mean Kingsford?"

--- api/curation.py
+++ api/curation.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from collections import defaultdict
 import ssl
+import nltk
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-lga_dic = defaultdict(str)
 
 def get_lga():
-    print(lga_dic)
+    lga_dic = defaultdict(str)
     if len(lga_dic) == 0:
         lga = "https://docs.google.com/spreadsheets/d/1RPJFmOFusUSS-Fu2nWids_SiH3CkAbKsml7IhgG-3Js/export?format=csv"
         df = pd.read_csv(lga)
@@ -15,31 +15,45 @@ def get_lga():
 
         for i in range(df.shape[0]):
             data = df.iloc[i]
-            suburb = data["Suburb/Town"]
+            suburb = data["Suburb/Town"].replace(u'\xa0', u' ').lstrip().capitalize()
             # print(suburb)
             council = data["Council Name"]
             council_check = council.split(' ')
             if council_check [-2] == 'City':
                 lga_dic[suburb] = ' '.join(council_check[:-2])
-
-    # print('Hello')
+    return lga_dic
     # print('test')
     # print(lga_dic)
 
-        # df = _df[_df['Suburb/Town'] == 'New South Wales']
-
-    # for i in range(df.shape[0]):
-    #     data = df.iloc[i]
-    #     city = data["LGA region"]
-    #     postcode = data["Postcode"]
-        # print("HEY LISTEN!",type(postcode))
-        # if city not in placelist:
-        #     continue
-        # if city in city_dict[postcode]:
-        #     continue
-        # else:
-        #     city_dict[postcode].append(city)
+def build_suburb_list():
+    lga_dic = get_lga()
+    ll = sorted((lga_dic.keys()))
+    # print(ll)
+    return ll
 
 
-get_lga()
+def typo_check():
+    '''
 
+    :return: the correction of potential typo: string
+    '''
+    err = 100
+    correction = ''
+    sub_list = build_suburb_list()
+
+    token = input("input suburb with error!\n")
+
+    for i in sub_list:
+        if err > nltk.edit_distance(token, i):
+            err = nltk.edit_distance(token, i)
+            correction = i
+
+    print(f'Do you mean {correction}?')
+    return correction
+
+
+
+
+# get_lga()
+# build_suburb_list()
+# typo_check()
