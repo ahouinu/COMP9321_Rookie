c63d3374bc02da1e46e6a0434a48aabb91524779
Miro <>
2018-05-28 20:27:08 +1000

    Change param into list. return {dict[poi_name]: (geo_x, geo_y)}

--- api/googlemap.py
+++ api/googlemap.py
@@ -1,4 +1,6 @@
 import googlemaps
+from collections import defaultdict
+
 
 gmaps = googlemaps.Client(key='AIzaSyDbdgCBSoXfep6LvrUsPRciIDc6ov9KceA')
 # target url: https://www.google.com.au/maps/search/-33.8305779,151.1646371
@@ -6,18 +8,21 @@ gmaps = googlemaps.Client(key='AIzaSyDbdgCBSoXfep6LvrUsPRciIDc6ov9KceA')
 def get_info(poi):
     '''
 
-    :param poi: string of poi name
-    :return: x and y geo_coordinate
+    :param poi: list of poi name.
+    :return: list of {dicts[poiname]: (x,y)}
     '''
+    geo_dic = dict()
 
-    geocode_test = gmaps.geocode(poi)
-    # geo_place = geocode_test[0]['geometry']['location']
+    for i in poi:
+        i = i.strip()
+        geocode_test = gmaps.geocode(i)
+        # geo_place = geocode_test[0]['geometry']['location']
 
-    x = geocode_test[0]['geometry']['location']['lat']
-    y = geocode_test[0]['geometry']['location']['lng']
+        x = geocode_test[0]['geometry']['location']['lat']
+        y = geocode_test[0]['geometry']['location']['lng']
+        geo_dic[i] = (x,y)
 
-    geo_dic = {'x':x,'y':y}
-
+    print(len(geo_dic))
     return geo_dic
 
 ## can be used to generate a target url:
@@ -26,4 +31,4 @@ def get_info(poi):
 
 ## TEST
 # print(url)
-# print(get_info('KINGSFORD SMITH OVAL'))
+# print(get_info(['KINGSFORD SMITH OVAL',' KINGSFORD SMITH PARK']))
